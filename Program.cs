dc8f47465e05580202c00bc9877fee24b87ac3fc
Ileana Gonzalez <ileanagonzalez@Ileanas-MacBook-Pro.local>
2026-07-02 09:31:23 -0600

    Convert word into Pig Latin: Done

--- Program.cs
+++ Program.cs
@@ -9,3 +9,33 @@ Console.Write("Please write your message: ");
 string message = Console.ReadLine()!;
 
 string[] words = message.Split(' ');
+
+string pigLatin = "";
+string encryptedM = "";
+
+string vowels = "aeiouAEIOU";
+
+foreach (string word in words)
+{
+    string pigWord = "";
+
+    if (vowels.Contains(word[0]))
+    {
+        pigWord = word + "way";
+    }
+    else
+    {
+        int constant = 0;
+
+        while (constant < word.Length && !vowels.Contains(word[constant]))
+        {
+            constant++;
+        }
+        string beginning = word.Substring(0, constant);
+        string rest = word.Substring(constant);
+
+        pigWord = rest + beginning + "ay";
+    }
+
+    pigLatin += pigWord + " ";
+}
